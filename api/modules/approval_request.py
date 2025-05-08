6c3b6f763aac326548fa3618412239a2df273bf1
patrickNachenga <patricknachenga@gmail.com>
2025-05-09 02:27:32 +0300

    save chnage requests

---
+++ api/modules/approval_request.py
@@ -0,0 +1,131 @@
+from datetime import datetime
+
+from django.core.exceptions import ImproperlyConfigured
+from django.db import transaction
+from rest_framework.exceptions import NotFound
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.views import APIView
+
+from api.serializers import ApprovalRequestSerializer, REQUEST_TYPE_SERIALIZER_IMPORTS, get_serializer_class
+from mnh_approval.pagination import CustomPagination
+from mnh_approval.response_codes import CustomResponse, STATUS_CODES
+from mnh_model.models import ApprovalRequest, ApprovalModule
+
+
+class ApprovalRequestView(APIView):
+    permission_classes = [AllowAny]
+    serializer_class = ApprovalRequestSerializer
+
+
+    def get(self, request, uid=None):
+        try:
+            if uid:
+                approval_request = ApprovalRequest.objects.filter(uid=uid, is_deleted=False).first()
+                if not approval_request:
+                    raise NotFound("Approval Request not found")
+
+                # Get related instance dynamically based on type
+                try:
+                    related_attr = approval_request.type.lower()
+                    related_instance = getattr(approval_request, related_attr, None)
+                except AttributeError:
+                    related_instance = None
+
+                # Attach it dynamically for serializer
+                setattr(approval_request, 'request_details', related_instance)
+
+                return CustomResponse.success(data=ApprovalRequestSerializer(approval_request).data)
+
+
+            search_query = request.GET.get('search', '').strip()
+            approval_request = ApprovalRequest.objects.filter(is_deleted=False)
+
+            if search_query:
+                approval_request = approval_request.filter(name__icontains=search_query)
+
+            if approval_request.exists():
+                return CustomPagination.paginate(self=self, results=approval_request, request=request)
+
+            return CustomResponse.errors(message="Approval Request not found", data=[])
+        except Exception as e:
+            return CustomResponse.server_error(message=f'Failed to Retrieve Approval Requests: {str(e)}', )
+
+    def post(self, request):
+        try:
+            with transaction.atomic():
+                uid = request.data.get('uid')
+                instance = None
+
+                # 1. Check if it's an update operation
+                if uid:
+                    try:
+                        instance = ApprovalRequest.objects.get(uid=uid)
+                    except ApprovalRequest.DoesNotExist:
+                        return CustomResponse.errors(message="Approval Request not found")
+
+                # 2. Handle base serializer
+                base_serializer = self.serializer_class(
+                    instance, data=request.data, partial=bool(instance)
+                )
+
+                # 3. Validate request type and load child serializer
+                request_type = request.data.get('type')
+                request_data = request.data.get('request_data', {})
+                if base_serializer.is_valid():
+                    if instance:
+                        instance = base_serializer.update(instance, base_serializer.validated_data)
+                    else:
+                        instance = base_serializer.save(created_by=request.user.id, updated_by=request.user.id)
+
+                    # 4. Save child serializer (specific request model)
+                    request_data['approval_request'] = instance.id
+
+                    try:
+                        serializer_class = get_serializer_class(request_type)
+                    except ImproperlyConfigured:
+                        return CustomResponse.errors(
+                            message=f"Request type '{request_type}' not supported",
+                            data=base_serializer.errors,
+                            code=STATUS_CODES['VALIDATION_ERROR']
+                        )
+
+                    child_serializer = serializer_class(data=request_data)
+                    if child_serializer.is_valid(raise_exception=True):
+                        print(child_serializer.validated_data)
+                        child_serializer.save()
+                    else:
+                        return CustomResponse.errors(
+                            message="Validation Failed",
+                            data=child_serializer.errors,
+                            code=STATUS_CODES["VALIDATION_ERROR"]
+                        )
+
+                    return CustomResponse.success(data=base_serializer.data)
+
+                return CustomResponse.errors(
+                    message="Validation Failed",
+                    data=base_serializer.errors,
+                    code=STATUS_CODES["VALIDATION_ERROR"]
+                )
+
+        except Exception as e:
+            return CustomResponse.server_error(
+                message=f"Failed to Change Approval Request: {str(e)}"
+            )
+
+    def delete(self, request, uid):
+        try:
+            with transaction.atomic():
+                """ Soft delete a Approval Request by UID """
+                approval_request = ApprovalRequest.objects.filter(uid=uid, is_deleted=False).first()
+                if not approval_request:
+                    return CustomResponse.errors(message="Approval Request Not Found or Deleted",)
+
+                approval_request.is_deleted = True
+                approval_request.deleted_at = datetime.now()
+                approval_request.deleted_by = request.user.id
+                approval_request.save()
+                return CustomResponse.success(message='Approval Request deleted successfully')
+
+        except Exception as e:
+            return CustomResponse.server_error(message="Something went wrong While Deleting Approval Request")
--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from importlib import import_module
+from django.core.exceptions import ImproperlyConfigured
 from rest_framework.validators import UniqueTogetherValidator
 
 
+from mnh_auth.serializers import UserSerializer
 from mnh_model.models import (
     ApprovalModule, ApprovalLevel, ApprovalAction,
     ApprovalModuleLevel, ApprovalRequest, RequestJeevaAccess, RequestInternetEmailAccess, ApprovalRequestStep,
@@ -10,13 +13,31 @@ from mnh_model.models import (
 )
 
 
+
+REQUEST_TYPE_SERIALIZER_IMPORTS = {
+    'INTERNET_EMAIL_ACCESS': 'api.serializers.RequestInternetEmailAccessSerializer',
+    'JEEVA_ACCESS': 'apps.mnh_approval.serializers.RequestJeeverAccessSerializer',
+}
+
+def get_serializer_class(request_type):
+    path = REQUEST_TYPE_SERIALIZER_IMPORTS.get(request_type)
+    if not path:
+        raise ImproperlyConfigured(f"No serializer configured for request_type '{request_type}'")
+    module_path, class_name = path.rsplit('.', 1)
+    module = import_module(module_path)
+    return getattr(module, class_name)
+
+
+
+
+
+
 class DirectorySerializer(serializers.ModelSerializer):
     departments = serializers.SerializerMethodField(read_only=True)
 
-
     class Meta:
         model = Directory
-        fields = ['uid', 'name', 'code','description', 'created_at', 'updated_at', 'departments']
+        fields = ['uid', 'name', 'code', 'description', 'created_at', 'updated_at', 'departments']
         read_only_fields = ['uid', 'created_at', 'updated_at']
         extra_kwargs = {
             'created_by': {'read_only': True},
@@ -41,13 +62,15 @@ class DirectorySerializer(serializers.ModelSerializer):
 
         return data
 
+
 class DepartmentSerializer(serializers.ModelSerializer):
     directory_uid = serializers.UUIDField(write_only=True)
     directory = DirectorySerializer(read_only=True)
 
     class Meta:
         model = Department
-        fields = ['uid', 'name', 'code', 'directory','directory_uid', 'description', 'is_active', 'created_at', 'updated_at']
+        fields = ['uid', 'name', 'code', 'directory', 'directory_uid', 'description', 'is_active', 'created_at',
+                  'updated_at']
         read_only_fields = ['uid', 'created_at', 'updated_at', 'directory_uid']
         extra_kwargs = {
             'created_by': {'read_only': True},
@@ -84,6 +107,7 @@ class DepartmentSerializer(serializers.ModelSerializer):
         validated_data.pop('directory_uid', None)
         return super().update(instance, validated_data)
 
+
 class ApprovalLevelSerializer(serializers.ModelSerializer):
     class Meta:
         model = ApprovalLevel
@@ -106,10 +130,11 @@ class ApprovalLevelSerializer(serializers.ModelSerializer):
 
         return data
 
+
 class ApprovalActionSerializer(serializers.ModelSerializer):
     class Meta:
         model = ApprovalAction
-        fields = ['uid', 'name', 'code','description', 'is_active', 'created_at', 'updated_at']
+        fields = ['uid', 'name', 'code', 'description', 'is_active', 'created_at', 'updated_at']
         read_only_fields = ['uid', 'created_at', 'updated_at']
         extra_kwargs = {
             'created_by': {'read_only': True},
@@ -137,13 +162,13 @@ class ApprovalModuleLevelSerializer(serializers.ModelSerializer):
 
     level = ApprovalLevelSerializer(read_only=True)
     action = ApprovalActionSerializer(read_only=True)
-    department=DepartmentSerializer(read_only=True)
+    department = DepartmentSerializer(read_only=True)
 
     class Meta:
         model = ApprovalModuleLevel
         fields = [
             'uid', 'module_uid', 'level_uid', 'level',
-            'action_uid', 'action', 'order','department','department_uid',
+            'action_uid', 'action', 'order', 'department', 'department_uid',
             'is_active', 'is_signatory', 'created_at', 'updated_at'
         ]
         read_only_fields = ['uid', 'created_at', 'updated_at']
@@ -203,16 +228,16 @@ class ApprovalModuleLevelSerializer(serializers.ModelSerializer):
         validated_data.pop('action_uid', None)
         validated_data.pop('department_uid')
 
-
         return super().update(instance, validated_data)
+
 
 class ApprovalModuleSerializer(serializers.ModelSerializer):
     approval_module_levels = serializers.SerializerMethodField()
 
     class Meta:
         model = ApprovalModule
-        fields = ['uid', 'name','description','approval_module_levels', 'created_at', 'updated_at']
-        read_only_fields = ['uid','approval_module_levels', 'created_at', 'updated_at']
+        fields = ['uid', 'name', 'description', 'approval_module_levels', 'created_at', 'updated_at']
+        read_only_fields = ['uid', 'approval_module_levels', 'created_at', 'updated_at']
         extra_kwargs = {
             'created_by': {'read_only': True},
             'updated_by': {'read_only': True},
@@ -239,6 +264,7 @@ class ApprovalModuleSerializer(serializers.ModelSerializer):
                 raise serializers.ValidationError("this module already exists.")
         return data
 
+
 class JeevaRoleSerializer(serializers.ModelSerializer):
     class Meta:
         model = JeevaRole
@@ -260,6 +286,7 @@ class JeevaRoleSerializer(serializers.ModelSerializer):
             if existing.exclude(uid=uid).exists():
                 raise serializers.ValidationError("this name and code already exists.")
         return data
+
 
 class JeevaPermissionSerializer(serializers.ModelSerializer):
     class Meta:
@@ -284,35 +311,45 @@ class JeevaPermissionSerializer(serializers.ModelSerializer):
         return data
 
 
-
 class RequestInternetEmailAccessSerializer(serializers.ModelSerializer):
+    approval_request = serializers.PrimaryKeyRelatedField(queryset=ApprovalRequest.objects.all())
     class Meta:
         model = RequestInternetEmailAccess
-        fields = ['uid', 'email', 'purpose', 'justification']
-        read_only_fields = ['uid']
+        fields = ['uid','approval_request', 'start_date', 'end_date', 'purpose']
+        read_only_fields = ['uid', 'created_at','is_read_term', 'updated_at']
+
 
 class ApprovalRequestSerializer(serializers.ModelSerializer):
-    data_json = serializers.JSONField(write_only=True)
     module_uid = serializers.UUIDField(write_only=True)
-    department_uid = serializers.UUIDField(write_only=True)
+    department_uid = serializers.UUIDField(write_only=True, required=True, allow_null=False)
 
-    module = serializers.SerializerMethodField(read_only=True)
-    department = serializers.SerializerMethodField(read_only=True)
+    type = serializers.ChoiceField(choices=ApprovalRequest.REQUEST_TYPES)
+    request_data = serializers.DictField(write_only=True)
+
+    module = ApprovalModuleSerializer(read_only=True)
+    department = DepartmentSerializer(read_only=True)
+
+    request_details = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = ApprovalRequest
         fields = [
-            'uid', 'request_type', 'title', 'description', 'status', 'start_date', 'end_date',
-            'requested_by', 'module_uid', 'department_uid', 'module', 'department', 'data_json',
-            'created_at', 'updated_at'
+            'uid', 'title', 'description', 'type', 'request_data',
+            'module_uid', 'department_uid','module', 'department', 'created_by',
+            'status', 'created_at', 'updated_at','request_details'
         ]
-        read_only_fields = ['uid', 'created_at', 'updated_at']
+        read_only_fields = ['uid', 'created_by','created_at', 'updated_at', 'status']
 
-    def get_module(self, obj):
-        return str(obj.module.name) if obj.module else None
+    def get_request_details(self, obj):
+        try:
+            inner_serializer = get_serializer_class(obj.type)
+            if not inner_serializer:
+                return None
+            child_instance = getattr(obj, 'request_details')  # e.g., obj.internet_email_access
+            return inner_serializer(child_instance).data
+        except AttributeError:
 
-    def get_department(self, obj):
-        return str(obj.department.name) if obj.department else None
+            return None
 
     def validate(self, data):
         module_uid = data.get('module_uid')
@@ -321,34 +358,27 @@ class ApprovalRequestSerializer(serializers.ModelSerializer):
         try:
             data['module'] = ApprovalModule.objects.get(uid=module_uid, is_deleted=False)
         except ApprovalModule.DoesNotExist:
-            raise serializers.ValidationError({"module_uid": "Invalid Module UID"})
-
+            raise serializers.ValidationError({"module_uid": "Invalid Module, not found or deleted"})
         try:
             data['department'] = Department.objects.get(uid=department_uid, is_deleted=False)
         except Department.DoesNotExist:
-            raise serializers.ValidationError({"department_uid": "Invalid Department UID"})
+            raise serializers.ValidationError({"department_uid": "Invalid Department, not found or deleted"})
 
         return data
 
     def create(self, validated_data):
-        request_type = validated_data.get('request_type')
-        data_json = validated_data.pop('data_json', {})
+        validated_data.pop('request_data')
+        validated_data.pop('module_uid')
+        validated_data.pop('department_uid')
+        return ApprovalRequest.objects.create(**validated_data)
 
-        module = validated_data.pop('module')
-        department = validated_data.pop('department')
+    def update(self, instance, validated_data):
+        validated_data.pop('request_data')
+        validated_data.pop('module_uid')
+        validated_data.pop('department_uid')
 
-        approval_request = ApprovalRequest.objects.create(
-            **validated_data, module=module, department=department
-        )
+        return super().update(instance, validated_data)
 
-        if request_type == 'internet_email':
-            RequestInternetEmailAccess.objects.create(
-                approval_request=approval_request,
-                **data_json
-            )
-        # add other request types here
-
-        return approval_request
 
 
 class ApprovalRequestStepSerializer(serializers.ModelSerializer):
@@ -364,4 +394,3 @@ class RequestJeevaAccessSerializer(serializers.ModelSerializer):
     class Meta:
         model = RequestJeevaAccess
         fields = '__all__'
-
